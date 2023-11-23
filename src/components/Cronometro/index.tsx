89b800bc3845080bbbe3280d242effbdb5c33361
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-23 17:30:12 -0300

    Renderizando tempo com useEffect

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -3,7 +3,7 @@ import { ITarefa } from '../../types/tarefa'
 import Button from '../Button'
 import style from './Cronometro.module.scss'
 import Relogio from './Relogio'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface CronometroProps {
     selecionado: ITarefa | undefined
@@ -11,9 +11,15 @@ interface CronometroProps {
 
 export function Cronometro({selecionado}: CronometroProps) {
     const [tempo, setTempo] = useState<Number>();
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            Tempo: {tempo?.toString()}
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
