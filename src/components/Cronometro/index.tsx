8a35c616c1eaba70e5e09f0e20044b35cffedfd9
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-23 16:40:53 -0300

    tempo para segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,10 @@
+import { tempoParaSegundos } from '../../common/utils/date'
 import Button from '../Button'
 import style from './Cronometro.module.scss'
 import Relogio from './Relogio'
 
 export function Cronometro() {
+    console.log('Coversao: ', tempoParaSegundos('01:01:01'))
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
