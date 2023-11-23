c75a06afaffaefd75b5294ab87ecc59273baab0d
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-23 13:54:36 -0300

    Criando cronometro

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,15 @@
+import Button from '../Button'
+import style from './Cronometro.module.scss'
+import Relogio from './Relogio'
+
+export function Cronometro() {
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            <div className={style.relogioWrapper}>
+                <Relogio />
+            </div>
+            <Button>Começar!</Button>
+        </div>
+    )
+}
