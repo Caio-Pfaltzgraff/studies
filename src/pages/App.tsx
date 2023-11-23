d826546946f67aa8b26ad15720bdc4161ce67d59
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-23 11:44:32 -0300

    Usando css module

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import Formulario from "../components/Formulario";
 import Lista from "../components/Lista";
-import './styles.scss'
+import style from './App.module.scss'
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Formulario />
       <Lista />
     </div>
